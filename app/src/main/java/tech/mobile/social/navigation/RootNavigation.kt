169b497b48722d7f524cd247313e48356dd78427
popudev <phuthinh53.it@gmail.com>
2024-04-20 14:22:04 +0700

    feat: change theme and css some screen

--- app/src/main/java/tech/mobile/social/navigation/RootNavigation.kt
+++ app/src/main/java/tech/mobile/social/navigation/RootNavigation.kt
@@ -1,50 +1,60 @@
 package tech.mobile.social.navigation
 
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import tech.mobile.social.Screens
 import tech.mobile.social.navigation.app.BottomNavigationBar
-import tech.mobile.social.presentation.auth.ForgotPasswordScreen
 import tech.mobile.social.presentation.auth.login.LoginRoute
 import tech.mobile.social.presentation.auth.register.RegisterRoute
-import tech.mobile.social.ui.theme.NavigationBarTheme
+
 
 @Composable
 fun RootNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screens.Auth.route) {
-        //auth navigation
-        navigation(
-            startDestination = Screens.Login.route, route = Screens.Auth.route
+    Scaffold() { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screens.Auth.route,
+            modifier = Modifier.padding(paddingValues.calculateBottomPadding())
         ) {
-            composable(route = Screens.Login.route) {
-                NavigationBarTheme {
-                    LoginRoute(navController)
-                }
-            }
-            composable(route = Screens.Register.route) {
-                NavigationBarTheme {
-//                    RegisterScreen(navController)
-                    RegisterRoute(navController = navController)
-                }
-            }
-            composable(route = Screens.ForgotPassword.route) {
-                NavigationBarTheme {
-                    ForgotPasswordScreen(navController)
-                }
-            }
-        }
+            //auth navigation
+            navigation(
+                startDestination = Screens.Login.route, route = Screens.Auth.route
+            ) {
+                composable(route = Screens.Login.route) {
 
-        //app navigation
-        navigation(
-            startDestination = Screens.Home.route, route = Screens.AppRoot.route
-        ) {
-            composable(Screens.Home.route) {
-                BottomNavigationBar(rootNavController = navController)
+                    LoginRoute(navController)
+
+                }
+                composable(route = Screens.Register.route) {
+
+                    RegisterRoute(navController = navController)
+
+                }
+                composable(route = Screens.ForgotPassword.route) {
+
+
+                }
+            }
+
+            //app navigation
+            navigation(
+                startDestination = Screens.Home.route, route = Screens.AppRoot.route
+            ) {
+                composable(Screens.Home.route) {
+                    BottomNavigationBar(rootNavController = navController)
+                }
             }
         }
     }
+
 }
--- app/src/main/java/tech/mobile/social/presentation/auth/forgot/ForgotScreen.kt
+++ app/src/main/java/tech/mobile/social/presentation/auth/forgot/ForgotScreen.kt
@@ -1,11 +1,11 @@
-package tech.mobile.social.presentation.auth.login
+package tech.mobile.social.presentation.auth.forgot
 
-import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mail
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,27 +13,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import tech.mobile.social.R
-import tech.mobile.social.presentation.utils.components.AuthTxtApp
-import tech.mobile.social.presentation.utils.components.BtnApp
 import tech.mobile.social.presentation.utils.components.EmailApp
+import tech.mobile.social.presentation.utils.components.InputApp
 import tech.mobile.social.presentation.utils.components.PasswordApp
-import tech.mobile.social.shared.UserState
 import tech.mobile.social.ui.theme.BtnColor
-import tech.mobile.social.ui.theme.NavigationBarTheme
 import tech.mobile.social.ui.theme.PrimaryColor
+import androidx.compose.material.icons.filled.*
 
 @Composable
-fun LoginScreen(
-    state: CombinedUiState,
-    actions: LoginActions,
+fun ForgotScreen(
+    state: ForgotState,
+    actions: ForgotActions,
 ) {
-    Log.i("STATE", state.userState.toString())
-    Log.i("STATE", state.loginState.toString())
 
     Surface(
         color = Color.White,
@@ -54,16 +49,47 @@ fun LoginScreen(
                 contentDescription = stringResource(id = R.string.logo),
                 modifier = Modifier.padding(vertical = 30.dp, horizontal = 50.dp)
             )
-            EmailApp(onValueChange = { actions.onEmailChange(it) })
-            PasswordApp(onValueChange = { actions.onPasswordChange(it) }, modifier = Modifier.padding(top = 10.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    label = { Text(text = "Email") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                )
+                Button(
+                    onClick = { },
+//                    colors = ButtonDefaults.buttonColors(BtnColor),
+//                    contentPadding = PaddingValues(horizontal = 20.dp, vertical = 20.dp),
+//                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
+//                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text("Xác nhận")
+                }
+            }
+
+            InputApp(
+                onValueChange = { },
+                imageVector = Icons.Filled.Key,
+                label = "Mã OTP",
+            )
+
+            InputApp(
+                onValueChange = { },
+                imageVector = Icons.Filled.Password,
+                label = "Mật khẩu mới",
+            )
+
             Button(
-                onClick = { actions.onLogin() },
+                onClick = { },
                 colors = ButtonDefaults.buttonColors(BtnColor),
                 contentPadding = PaddingValues(horizontal = 50.dp, vertical = 20.dp),
                 modifier = Modifier.padding(top = 30.dp, bottom = 10.dp),
                 shape = RoundedCornerShape(10.dp)
             ) {
-                Text("Đăng nhập")
+                Text("Xác nhận")
             }
 
             Row(
@@ -71,7 +97,7 @@ fun LoginScreen(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(text = "Bạn chưa có tài khoản ?")
-                TextButton(onClick = { actions.navRegister() }) {
+                TextButton(onClick = { }) {
                     Text(text = "Đăng ký", color = PrimaryColor)
                 }
             }
@@ -81,11 +107,11 @@ fun LoginScreen(
 }
 
 @Composable
-@Preview(name = "Login2")
-fun Login2ScreenPreview() {
-    LoginScreen(
-        state = CombinedUiState(LoginState(), UserState()),
-        actions = LoginActions()
+@Preview(name = "Forgot")
+private fun ForgotScreenPreview() {
+    ForgotScreen(
+        state = ForgotState(),
+        actions = ForgotActions()
     )
 }
 
